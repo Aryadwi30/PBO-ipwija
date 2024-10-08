05882c1751b4ef2f63fea7fc75181b21c8ef092d
Aryadwi30 <sitiarya2@gmail.com>
2024-10-08 09:30:28 +0700

    polimorfishme.js

--- polimorfishme.js
+++ polimorfishme.js
@@ -1,3 +1,4 @@
+
 // Definisi kelas induk
 class Orang {
     tampilkanPeran() {
@@ -18,11 +19,19 @@ class Dosen extends Orang {
     }
 }
 
+class Anak extends Orang {
+    tampilkanPeran(){
+        console.log("peran: anak");
+    }
+}
+
 // Menggunakan polimorfisme
 const orang1 = new Orang();
 const mahasiswa1 = new Mahasiswa();
 const dosen1 = new Dosen();
+const anak1 = new Anak();
 
 orang1.tampilkanPeran();    // Output: Peran: Orang biasa
 mahasiswa1.tampilkanPeran(); // Output: Peran: Mahasiswa
 dosen1.tampilkanPeran();     // Output: Peran: Dosen
+anak1.tampilkanPeran();  //output: peran: anak
